4ebe60f96fde4b629b14c563c43906ac905171f8
FreyrLin <fan.lin@tieto.com>
2013-09-02 11:16:18 +0800

    test case modify

--- unittest/TestConn.c
+++ unittest/TestConn.c
@@ -8,24 +8,32 @@ START_TEST (test_conn_set)
     conn_t *conn = malloc(sizeof(conn_t));
     char* url = malloc(MAX_STRING);
 
-    strcpy(url, "www.nsn.com");
+    strcpy(url, "www.google.com");
     conn_set(conn, url);
-    ck_assert_str_eq(conn->host, "www.nsn.com");
+    ck_assert_str_eq(conn->host, "www.google.com");
     ck_assert_int_eq(conn->port, 80);
     ck_assert_str_eq(conn->dir, "/");
     ck_assert_str_eq(conn->file, "");
     ck_assert_str_eq(conn->user, "");
     ck_assert_str_eq(conn->pass, "");
 
-    //strcpy(url, "www.nsn.com/folder/news/index.html");
-    //conn_set(conn, url);
-    //fprintf(stderr, "'%s' -> \nhost: %s \nport: %d \ndir: %s \nfile: %s \nuser: %s \npwd: %s \n",
-    //        url, conn->host, conn->port, conn->dir, conn->file, conn->user, conn->pass);
+    strcpy(url, "www.yahoo.com/folder/news/index.html");
+    conn_set(conn, url);
+    ck_assert_str_eq(conn->host, "www.yahoo.com");
+    ck_assert_int_eq(conn->port, 80);
+    ck_assert_str_eq(conn->dir, "/folder/news/");
+    ck_assert_str_eq(conn->file, "index.html");
+    ck_assert_str_eq(conn->user, "");
+    ck_assert_str_eq(conn->pass, "");
 
-    //strcpy(url, "http://falin:abcacb@www.nsn.com:99/folder/older/index.html");
-    //conn_set(conn, url);
-    //fprintf(stderr, "'%s' -> \nhost: %s \nport: %d \ndir: %s \nfile: %s \nuser: %s \npwd: %s \n",
-    //        url, conn->host, conn->port, conn->dir, conn->file, conn->user, conn->pass);
+    strcpy(url, "http://u5er:passw0rd@www.yahoo.com:8090/folder/older/index.html");
+    conn_set(conn, url);
+    ck_assert_str_eq(conn->host, "www.yahoo.com");
+    ck_assert_int_eq(conn->port, 8090);
+    ck_assert_str_eq(conn->dir, "/folder/older/");
+    ck_assert_str_eq(conn->file, "index.html");
+    ck_assert_str_eq(conn->user, "u5er");
+    ck_assert_str_eq(conn->pass, "passw0rd");
 
     free(conn);
     free(url);
